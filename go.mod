a0932f31a509d3746d6ae6e3bfbc4792d01c0836
Sullivan Prellwitz <sullivan.prell@gmail.com>
2020-10-22 09:54:55 -0500

    Update to support CORS policy problems

--- go.mod
+++ go.mod
@@ -5,7 +5,9 @@ go 1.15
 require (
 	github.com/99designs/gqlgen v0.13.0
 	github.com/couchbase/gocb/v2 v2.1.6
-	github.com/dgraph-io/ristretto v0.0.3 // indirect
+	github.com/dgraph-io/ristretto v0.0.3
+	github.com/go-chi/chi v4.1.2+incompatible
 	github.com/joho/godotenv v1.3.0
+	github.com/rs/cors v1.7.0
 	github.com/vektah/gqlparser/v2 v2.1.0
 )
--- server.go
+++ server.go
@@ -8,7 +8,11 @@ import (
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
+	"github.com/gorilla/websocket"
+	"github.com/rs/cors"
 )
 
 const defaultPort = "8080"
@@ -18,12 +22,36 @@ func main() {
 	if port == "" {
 		port = defaultPort
 	}
+	router := chi.NewRouter()
+
+	// Add CORS middleware around every request
+	// See https://github.com/rs/cors for full option listing
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:8080"},
+		AllowCredentials: true,
+		Debug:            false,
+	}).Handler)
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	srv.AddTransport(&transport.Websocket{
+		Upgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				// Check against your desired domains here
+				return r.Host == "*"
+			},
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+		},
+	})
+
+	router.Handle("/", playground.Handler("GraphQL Playground", "/query"))
+	// router.Handle("/", handler.Playground("Starwars", "/query")) // MODIFIED THIS.
+	router.Handle("/query", srv)
+
+	// router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	// router.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
